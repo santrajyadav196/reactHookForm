ebc98b0b1af61dc86b7492a24dd0dfa610c4be8b
santrajyadav196 <santrajyadav196@gmail.com>
2021-12-17 19:36:57 +0530

    react hook form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useForm} from "react-hook-form";
+
+import "./App.css";
 
 function App() {
+  const {
+    register,
+    formState: {errors},
+    handleSubmit,
+  } = useForm();
+  console.log(errors);
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='mb-3'>
+            <label htmlFor='exampleFormControlInput1' class='form-label'>
+              Name
+            </label>
+            <input
+              type='text'
+              className='form-control'
+              id='exampleFormControlInput1'
+              name='name'
+              placeholder='Full Name'
+              {...register("name", {
+                required: true,
+                minLength: 4,
+              })}
+            />
+            <span className='form-text text-danger'>
+              {errors.name?.type === "required" && "First name is required"}
+            </span>
+            <span className='form-text text-danger'>
+              {errors.name?.type === "minLength" &&
+                "Minimum character should be 4"}
+            </span>
+          </div>
+          <button type='submit' className='btn btn-primary'>
+            Submit
+          </button>
+        </form>
       </header>
     </div>
   );
